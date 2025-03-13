f97830c804777663d0d2c08df3d7396e1824feb6
Patrick Goodwin <pattygsocials@gmail.com>
2025-03-13 15:12:18 -0400

    Create booleanChecker.js

---
+++ JavaScript/booleanChecker.js
@@ -0,0 +1,27 @@
+let testRuns = 500;
+
+function getRandomBoolean() {
+    return Math.random() < 0.5;
+}
+
+function generateExpression() {
+    const x = Array.from({ length: 5 }, getRandomBoolean);
+    
+    // Expression format 1
+    const ex1 = `((${x[0]} && ${x[1]} && ${x[2]}) || (${x[3]} && ${x[4]}))`;
+    
+    // Expression format 2
+    const ex2 = `(${x[0]} && ${x[1]} && ${x[2]} || ${x[3]} && ${x[4]})`;
+    
+    return eval(ex1) === eval(ex2);
+}
+
+let allTrue = true;
+for (let i = 0; i < testRuns; i++) {
+    if (!generateExpression()) {
+        allTrue = false;
+        break;
+    }
+}
+
+console.log(allTrue);
